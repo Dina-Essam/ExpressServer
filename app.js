abdacf617869b5ec4784395fbff83e8f56a101a6
Dina <Dina.essam.22.4.97@gmail.com>
2023-02-17 16:46:44 +0200

    filter paths

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false })); // next in the end so it will enter middleware in line
 
-app.use(adminRoutes);
+app.use('/admin' , adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {

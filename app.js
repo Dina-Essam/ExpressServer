e4f59546ba19ddcaea4e2b0afde1759c6359026a
Dina <Dina.essam.22.4.97@gmail.com>
2023-02-18 00:35:58 +0200

    using pug as template engin

--- app.js
+++ app.js
@@ -8,14 +8,16 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.set('view engine' , 'pug'); //using temple engine pug
+app.set('views' , 'views/pug'); // the html files directory
+
 app.use(bodyParser.urlencoded({ extended: false })); // next in the end so it will enter middleware in line
 app.use(express.static(path.join(rootDir , 'public')));
 
-app.use('/admin' , adminRoutes);
+app.use('/admin' , adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404);
-    res.sendFile(path.join(rootDir , 'views', '404.html'));
+    res.status(404).render('404');
 });
 app.listen(3000);

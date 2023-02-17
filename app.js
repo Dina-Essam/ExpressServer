499ddecf049265161d108d06f655a4ce3945f34c
Dina <Dina.essam.22.4.97@gmail.com>
2023-02-18 01:55:03 +0200

    using ejs as template engin

--- app.js
+++ app.js
@@ -5,19 +5,29 @@ const rootDir = require("./util/path");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
-// app.set('view engine' , 'pug'); //using temple engine pug
-// app.set('views' , 'views/pug'); // the html files directory
-app.engine('hbs', expressHbs.engine({
-    defaultLayout:'main-layout',
-    extname:'hbs',
-    layoutsDir:'views/handlebars/layouts'
-}));
-app.set("view engine", 'hbs'); //using temple engine handlebars
-app.set("views", "views/handlebars"); // the html files directory
+/**Using pug template engine
+ app.set('view engine' , 'pug'); //using template engine pug
+ app.set('views' , 'views/pug'); // the html files directory 
+*/
+
+
+/**
+ *  Using Handlebars template engine
+
+    const expressHbs = require('express-handlebars');
+    app.engine('hbs', expressHbs.engine({
+        defaultLayout:'main-layout',
+        extname:'hbs',
+        layoutsDir:'views/handlebars/layouts'
+    }));
+    app.set("view engine", 'hbs'); //using template engine handlebars
+    app.set("views", "views/handlebars"); // the html files directory
+*/
+app.set("view engine", 'ejs'); //using template engine ejs
+app.set("views", "views/ejs"); // the html files directory
 
 app.use(bodyParser.urlencoded({ extended: false })); // next in the end so it will enter middleware in line
 app.use(express.static(path.join(rootDir, "public")));

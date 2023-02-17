61ab250a337548a6eb5f62e881b77b70a25e3858
Dina <Dina.essam.22.4.97@gmail.com>
2023-02-17 16:21:43 +0200

    filter http methods

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const http = require('http');
-
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-const server = http.createServer();
+app.use(bodyParser.urlencoded({ extended: false })); // next in the end so it will enter middleware in line
 
-server.listen(3000);
+app.get("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.get("/", (req, res, next) => {
+  res.send("<h1>Hello from Express!</h1>");
+});
+
+app.listen(3000);

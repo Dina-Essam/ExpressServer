dc5177ca9038fae6ac449f27cb84fe843a91d5d1
Dina <Dina.essam.22.4.97@gmail.com>
2023-02-17 16:35:21 +0200

    add routes in another files

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false })); // next in the end so it will enter middleware in line
 
-app.get("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.get("/", (req, res, next) => {
-  res.send("<h1>Hello from Express!</h1>");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);

8fc65381201430ea3f320408c316d08e6a4f42ee
Dina <Dina.essam.22.4.97@gmail.com>
2023-02-18 03:02:36 +0200

    enhancement

--- models/product.js
+++ models/product.js
@@ -1,33 +1,32 @@
 const fs = require("fs");
 const path = require("path");
 const rootDir = require("../util/path");
+const p = path.join(rootDir, "data", "products.json");
+
+const getProductsFromFile = (callback) => {
+  fs.readFile(p, (err, fileContent) => {
+    let products = [];
+    if (!err) {
+      products = JSON.parse(fileContent);
+    }
+    return callback(products);
+  });
+};
 
 module.exports = class product {
   constructor(title) {
     this.title = title;
   }
   save() {
-    const p = path.join(rootDir, "data", "products.json");
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile((products) => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err)=>{
+      fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
       });
     });
   }
 
   static fetchAll(callback) {
-    const p = path.join(rootDir, "data", "products.json");
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
-      return callback(products);
-    });
+    getProductsFromFile(callback);
   }
 };
